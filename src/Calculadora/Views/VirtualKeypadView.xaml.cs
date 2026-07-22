f1e77c3118cb9fe5ca7d10d9c666dba254ceeddd
IAMS2003 <andresmontoya2504@gmail.com>
2026-07-22 16:14:27 -0500

    fix: Agrega archivos code-behind de StatisticsView y VirtualKeypadView para carga correcta de XAML en WPF

---
+++ src/Calculadora/Views/VirtualKeypadView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Calculadora.Views
+{
+    public partial class VirtualKeypadView : UserControl
+    {
+        public VirtualKeypadView()
+        {
+            InitializeComponent();
+        }
+    }
+}
